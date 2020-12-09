6675d72c28589a4ef41ec9ffab14e7ef6d4a5430
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-09 17:37:55 +0100

    Blocco 1(3)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@
 $(function () {
   //JSNACK BLOCCO 1
 
+
+
+
   //N1  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 
   const userNumber1 = Number(prompt("Inserisci un numero"));
@@ -12,19 +15,48 @@ $(function () {
 
   // /N1
 
+
+
+
+
   //N2  L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga
 
   const word1 = prompt ('Inserisci la prima parola');
   const word2 = prompt ('Inserisci la seconda parola');
 
   word1.length < word2.length ? console.log('second word is longer') : word1.length > word2.length ? console.log('first word is longer') : console.log('they got the same length');
-
-
   // /N2
 
-  //N3
+
+
+
+
+  //N3 Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while
+
+  let i = 0;
+  let sum = 0;
+  while (i < 5) {
+    const userNumber = Number(prompt("Insert a number"));
+    sum += userNumber;
+    i++;
+  }
+  console.log(sum);
+
   // /N3
-  //N4
+
+
+
+  //N4  In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa
+  
+
+  const partyList = ['marcolino', 'piripicchio', "leandro"];
+
+  const userName = prompt('Insert your name').toLowerCase();
+
+  partyList.includes(userName) ? console.log('Puoi partecipare alla festa') : console.log('Non puoi partecipare alla festa');
+
+  
+  
   // /N4
   // /JSNACK BLOCCO 1
 });
