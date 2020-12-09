7207f7aada61114ab0800f15b0a10e1685acc991
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-09 14:39:15 +0100

    Added main files

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Opzione deprecata da JQ 3.0
+// Codice viene eseguito solo quando documento e' carico
+$(document).ready(function () {
+  //codice jQuery
+
+
+  // Variabili con valore
+
+  // Variabili senza valore
+
+  // Functions
+});
+
+// Opzione piu recente
+// Codice viene eseguito solo quando documento e' carico
+$(function () {
+  //codice jQuery
+  
+  
+  // Variabili con valore
+
+  // Variabili senza valore
+  
+  // Functions
+
+});
