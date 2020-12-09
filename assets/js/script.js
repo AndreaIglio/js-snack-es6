5b986432953986e940e447ac71ff1598a702f59b
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-09 17:20:43 +0100

    Blocco 1(1,2)
    Using ternary operator and Const variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-// Opzione deprecata da JQ 3.0
-// Codice viene eseguito solo quando documento e' carico
-$(document).ready(function () {
-  //codice jQuery
 
-
-  // Variabili con valore
-
-  // Variabili senza valore
-
-  // Functions
-});
-
-// Opzione piu recente
 // Codice viene eseguito solo quando documento e' carico
 $(function () {
-  //codice jQuery
-  
-  
-  // Variabili con valore
+  //JSNACK BLOCCO 1
 
-  // Variabili senza valore
-  
-  // Functions
+  //N1  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 
+  const userNumber1 = Number(prompt("Inserisci un numero"));
+  const userNumber2 = Number(prompt("Inserisci un numero"));
+
+  userNumber1 > userNumber2 ? console.log("Number 1 win") : userNumber1 == userNumber2 ? console.log("Numbers are the same") : console.log("Number 2 win");
+
+  // /N1
+
+  //N2  L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga
+
+  const word1 = prompt ('Inserisci la prima parola');
+  const word2 = prompt ('Inserisci la seconda parola');
+
+  word1.length < word2.length ? console.log('second word is longer') : word1.length > word2.length ? console.log('first word is longer') : console.log('they got the same length');
+
+
+  // /N2
+
+  //N3
+  // /N3
+  //N4
+  // /N4
+  // /JSNACK BLOCCO 1
 });
